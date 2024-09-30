a15a7a35d560758ce10a038bd40ec40018e6f8dd
filebin <alex.mousse9@gmail.com>
2024-09-30 21:15:40 +0300

    content manager frontend base

---
+++ content-manager-frontend/src/components/app-header/AppIcon.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { FileBadge2 } from 'lucide-vue-next'
+</script>
+
+<template>
+    <FileBadge2 class="h-6 w-6" />
+    <span class="sr-only">ContentManager</span>
+</template>
---
+++ content-manager-frontend/src/views/RecentPosts.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+  <main>
+    <div>
+      <Button>Click me</Button>
+      <ThemeSwitch></ThemeSwitch>
+    </div>
+  </main>
+</template>
